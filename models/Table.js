723c960d2d737b715357266097ea59cea817d994
unknown <msvendsentan@gmail.com>
2018-10-22 18:27:36 -0400

    add db relationship

--- models/Table.js
+++ models/Table.js
@@ -4,7 +4,13 @@ const Schema = mongoose.Schema;
 const tableSchema = new Schema({
     number: { type: String, required: true },
     password: { type: String, required: true },
-    capacity: { type: Number }
+    capacity: { type: Number, required: true },
+    guests: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Guest"
+        }
+    ]
 });
 
 const Table = mongoose.model("Table", tableSchema);
